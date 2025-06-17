35aba00837bf5c2bbc17f770301c9bce8caec6bc
krzysztof25127 <25127@edu.amw.gdynia.pl>
2025-06-17 07:45:26 +0200

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
